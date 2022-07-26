66c805aacc426b9e1ca5ffa7b1b301044b6b7fdd
Katy Fernelius <kkin001@gmail.com>
2022-07-26 16:38:37 -0500

    initial  commit

---
+++ index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+import express from 'express';
+import cors from 'cors';
+import { MongoClient, ObjectId } from 'mongodb';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const CONNECTION_STRING =
+  'mongodb+srv://kferne3:Violanerd12!@yoga-flow.g11hl.mongodb.net/?retryWrites=true&w=majority';
+
+//MIDDLEWARE//
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
+
+MongoClient.connect(CONNECTION_STRING).then(async (client) => {
+  console.log('Connected to database...');
+  const db = client.db('flows');
+  const flowCollection = db.collection('saved-flows');
+
+  app.get('/', async (req, res) => {
+    await db
+      .collection('saved-flows')
+      .find()
+      .toArray()
+      .then((results) => {
+        console.log(results);
+        res.send(results);
+      })
+      .catch((error) => console.error(error));
+  });
+
+  //TODO-Add validation to the post method so that flows posted in there include the necessary data
+  //TODO-figure out routes and mongoose for the get/post/delete
+
+  app.post('/flows', (req, res) => {
+    flowCollection
+      .insertOne(req.body)
+      .then((result) => {
+        console.log(result);
+        res.redirect('/');
+      })
+      .catch((error) => console.log(error));
+  });
+});
